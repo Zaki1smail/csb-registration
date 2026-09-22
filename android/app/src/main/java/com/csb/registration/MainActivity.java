2e8b299627de120a5ed8a044772428037a160cf5
Zaki1smail <madjid4sm@gmail.com>
2026-09-22 21:34:57 +0100

    Update MainActivity.java

--- android/app/src/main/java/com/csb/registration/MainActivity.java
+++ android/app/src/main/java/com/csb/registration/MainActivity.java
@@ -1,13 +1,12 @@
 package com.csb.registration;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     private static final String APP_URL = "https://csb-registration.vercel.app";
 
